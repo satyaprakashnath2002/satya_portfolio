ac09e3e875392abc78f42d0b0f39dfb85ff4be58
satyaprakashnath2002 <satyaprakashnath2002@gmail.com>
2026-02-13 00:44:42 +0530

    Fix mobile responsiveness for Hero and Skills section

--- src/components/canvas/ball.tsx
+++ src/components/canvas/ball.tsx
@@ -7,55 +7,41 @@ import {
 } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-
 import CanvasLoader from "../loader";
 
-// ---------------- TYPES ----------------
 type BallProps = {
   imgUrl: string;
+};
+
+const Ball = ({ imgUrl }: BallProps) => {
+  const [decal] = useTexture([imgUrl]);
+
+  return (
+    <Float speed={1.5} rotationIntensity={1} floatIntensity={1.5}>
+      <ambientLight intensity={0.4} />
+      <directionalLight position={[0, 0, 0.5]} />
+
+      <mesh scale={2.5}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial color="#fff8eb" flatShading />
+
+        <Decal position={[0, 0, 1]} map={decal} />
+      </mesh>
+    </Float>
+  );
 };
 
 type BallCanvasProps = {
   icon: string;
 };
 
-// ---------------- BALL COMPONENT ----------------
-const Ball = ({ imgUrl }: BallProps) => {
-  const [decal] = useTexture([imgUrl]);
-
-  return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      {/* Lights */}
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-
-      {/* Mesh */}
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color="#fff8eb"
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-
-        {/* Logo / Icon */}
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          map={decal}
-        />
-      </mesh>
-    </Float>
-  );
-};
-
-// ---------------- BALL CANVAS ----------------
 const BallCanvas = ({ icon }: BallCanvasProps) => {
   return (
     <Canvas
-      frameloop="demand"
+      frameloop="always"
+      dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
+      camera={{ position: [0, 0, 5], fov: 45 }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
